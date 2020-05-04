6adfd9b8a75b8012b81586cb2532a5aacc313998
Apurv Dhadankar <53594814+apurvdhadankar@users.noreply.github.com>
2020-05-04 11:39:20 +0530

    Create translator.py

---
+++ translator.py
@@ -0,0 +1,24 @@
+import tkinter as tk 
+from googletrans import Translator
+
+win = tk.Tk()
+win.title("Translator")
+win.geometry("300x150")
+
+def translation():
+    word = entry.get()
+    translator = Translator(service_urls=['translate.google.com'])
+    translation1 = translator.translate(word,dest="marathi")
+    label1 = tk.Label(win,text=f'Translated In Marathi : {translation1.text}',bg="yellow")
+    label1.grid(row=2,column=0)
+
+label = tk.Label(win,text="Enter Word : ")
+label.grid(row=0,column=0,sticky="W")
+
+entry = tk.Entry(win)
+entry.grid(row=1,column=0)
+
+button = tk.Button(win,text="Translate",command=translation)
+button.grid(row=1,column=2)
+
+win.mainloop()
